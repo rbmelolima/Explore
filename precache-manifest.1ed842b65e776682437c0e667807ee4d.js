a7b7a7391f9de9760b814fe55e838931e67752a4
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-07-12 13:30:09 -0300

    Updates

--- precache-manifest.1ed842b65e776682437c0e667807ee4d.js
+++ precache-manifest.1ed842b65e776682437c0e667807ee4d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "767ac6d3919541ced2c78cd70eb6b246",
+    "revision": "6ed04b5d65eaf58c67939524dfb365ef",
     "url": "/Explore/index.html"
   },
   {
-    "revision": "627ae6a18f177bab938e",
+    "revision": "9456e35b4278fb4f20f8",
     "url": "/Explore/static/css/main.6ef01283.chunk.css"
   },
   {
-    "revision": "5082db65e10e30941d4b",
-    "url": "/Explore/static/js/2.c43cae4c.chunk.js"
+    "revision": "3fe89b4f5604f11c5475",
+    "url": "/Explore/static/js/2.d9aa312b.chunk.js"
   },
   {
     "revision": "0d10b9de47c8023f7fd9d830b32d5242",
-    "url": "/Explore/static/js/2.c43cae4c.chunk.js.LICENSE.txt"
+    "url": "/Explore/static/js/2.d9aa312b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "627ae6a18f177bab938e",
-    "url": "/Explore/static/js/main.03a70912.chunk.js"
+    "revision": "9456e35b4278fb4f20f8",
+    "url": "/Explore/static/js/main.ba49e38a.chunk.js"
   },
   {
     "revision": "3df2cff7d311b5e69130",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Explore/precache-manifest.b43e5ef46cff490d865a3b2aeb6fb0a3.js"
+  "/Explore/precache-manifest.1ed842b65e776682437c0e667807ee4d.js"
 );
 
 self.addEventListener('message', (event) => {
